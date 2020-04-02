9e50d3f284dd61045c213c732b8c257f4a3f114c
khacchungory <ochung3@gmail.com>
2020-04-03 00:52:56 +0700

    finish screen 1

--- screen1/lib/HomePage.dart
+++ screen1/lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:screen1/FadeAnimation.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -8,12 +9,173 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Widget makeCategory({isActive, title}) {
+    return AspectRatio(
+      aspectRatio: isActive ? 3 : 2.0 / 1,
+      child: Container(
+        margin: EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          color: isActive ? Colors.yellow[700] : Colors.white,
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: Align(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: isActive ? Colors.white : Colors.grey[500],
+              fontSize: 18,
+              fontWeight: isActive ? FontWeight.bold : FontWeight.w100,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeItem({image}) {
+    return AspectRatio(
+      aspectRatio: 1 / 1.4,
+      child: GestureDetector(
+        child: Container(
+          margin: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                colors: [
+                  Colors.black.withOpacity(0.9),
+                  Colors.black.withOpacity(0.3),
+                ],
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Container(),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[100],
+        elevation: 0,
+        brightness: Brightness.light,
+        leading: Icon(null),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_basket),
+            color: Colors.grey[800],
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(
+                    1,
+                    Text(
+                      'Fashion Delivery',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey[80],
+                          fontSize: 30),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        FadeAnimation(
+                          1,
+                          makeCategory(isActive: true, title: 'Short'),
+                        ),
+                        FadeAnimation(
+                          1,
+                          makeCategory(isActive: false, title: 'Short'),
+                        ),
+                        FadeAnimation(
+                          1,
+                          makeCategory(isActive: false, title: 'Short'),
+                        ),
+                        FadeAnimation(
+                          1,
+                          makeCategory(isActive: false, title: 'Short'),
+                        ),
+                        FadeAnimation(
+                          1,
+                          makeCategory(isActive: false, title: 'Short'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            FadeAnimation(
+              1,
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'Free Delivery',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 32),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    makeItem(image: 'assets/images/two.jpg'),
+                    makeItem(image: 'assets/images/three.jpg'),
+                    makeItem(image: 'assets/images/for.jpg'),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
